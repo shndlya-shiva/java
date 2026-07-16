6e63effc8fa82c4237fed45d2558a6f597da74de
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-07-17 00:37:14 +0530

    Diff data types

--- hello.java
+++ hello.java
@@ -1,5 +1,34 @@
 class test{
     public static void main(String[] args){
-       System.out.println("Hello World");
+    // first java program
+        System.out.println("Hello World");
+    }
+}
+class test1{
+    public static void main(String[] args){
+     //diff data types
+        int num = 5;
+        int num1 = 10;
+        int num2 = 15;
+        int result = num1 + num2 + num;
+        float num3 = 5.5f;
+        byte num4 = -127;
+        short num5 = 32767;
+        long num6 = 1000000000L;
+        char character = 'A';
+        String character2 = "Shiva";
+        boolean b = true;
+        System.out.println(num);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(b);
+        System.out.println(result);
+        System.out.println(3 + 5);
+        System.out.println(7 + 2);
+        System.out.println(character);
+        System.out.println(character2);
+        
     }
 }
