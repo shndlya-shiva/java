4529f70e12957c90b1fbccac285681c6ab46e8da
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-07-17 01:01:52 +0530

    Sum of two int no.

--- hello.java
+++ hello.java
@@ -11,24 +11,9 @@ class test1{
         int num1 = 10;
         int num2 = 15;
         int result = num1 + num2 + num;
-        float num3 = 5.5f;
-        byte num4 = -127;
-        short num5 = 32767;
-        long num6 = 1000000000L;
-        char character = 'A';
-        String character2 = "Shiva";
-        boolean b = true;
         System.out.println(num);
-        System.out.println(num3);
-        System.out.println(num4);
-        System.out.println(num5);
-        System.out.println(num6);
-        System.out.println(b);
-        System.out.println(result);
-        System.out.println(3 + 5);
-        System.out.println(7 + 2);
-        System.out.println(character);
-        System.out.println(character2);
-        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
     }
 }
