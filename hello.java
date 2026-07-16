f68f2db1b3f9d9c51791c9139190f9eb44df5945
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-07-17 00:32:11 +0530

    New file

---
+++ hello.java
@@ -0,0 +1,34 @@
+class test{
+    public static void main(String[] args){
+        //diff data types
+        int num = 5;
+        int num1 = 10;
+        int num2 = 15;
+        int result = num1 + num2 + num;
+        float num3 = 5.5f;
+        byte num4 = -127;
+        short num5 = 32767;
+        long num6 = 1000000000L;
+        char character = 'A';
+        String character2 = "Shiva";
+        boolean b = true;
+        System.out.println(num);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(b);
+        System.out.println(result);
+        System.out.println(3 + 5);
+        System.out.println(7 + 2);
+        System.out.println(character);
+        System.out.println(character2);
+    }
+}
+public class hello {
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
