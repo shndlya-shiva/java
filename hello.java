c3c100d14083f5a7fe62245857e85da299213ebc
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-07-21 16:26:33 +0530

    new class

--- hello.java
+++ hello.java
@@ -345,5 +345,16 @@ class test17 {
             default:
                 System.out.println("Invalid Day");
         }
+        //other way of writing switch statement
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Invalid Day");
+        }
     }
 }
