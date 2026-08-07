f47ff986b8729456419cf874d90f844b60c3e971
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-07 22:13:34 +0530

    Superclass

--- Calc.java
+++ Calc.java
@@ -1,4 +1,6 @@
 public class Calc {
+// Here in this Calc class we are creating two methods add and subtract
+// Calc is a superclass and AdvCalc is a subclass that extends the Calc class    
     public int add(int a, int b) {
         return a + b;
     }
