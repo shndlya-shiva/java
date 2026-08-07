0432d8ef6f235a3e2ab4ba2275a69bfddd4c5ee3
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-07 22:14:01 +0530

    Subclass

--- AdvCalc.java
+++ AdvCalc.java
@@ -1,4 +1,6 @@
 public class AdvCalc extends Calc {
+// Here in this AdvCalc class we are extending the Calc class and adding two more methods multiply and divide
+// AdvCalc is a subclass of Calc class and it inherits the methods add and subtract from Calc class
     public int multiply(int a, int b) {
         return a * b;
     }
