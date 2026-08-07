e4556ccecc6fe8ee57fd65eef7cf0acecad2fd13
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-07 22:42:14 +0530

    Multi level Inheritance

--- SciCalc.java
+++ SciCalc.java
@@ -2,7 +2,8 @@ public class SciCalc extends AdvCalc {
 // Here in this SciCalc class we are extending the AdvCalc class and adding two more methods
 // square and squareRoot
 // SciCalc is a subclass of AdvCalc class and it inherits the methods add, subtract, multiply and divide from AdvCalc class
-    public int square(int a) {
+// Multilevel inheritance is demonstrated here where SciCalc inherits from AdvCalc which in turn inherits from Calc class
+public int square(int a) {
         return a * a;
     }
     
