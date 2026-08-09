1e7cc49cff63526e06ddd8c4b74042e5c7db94ff
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-09 23:12:23 +0530

    Adding Parametirsed Construrctor

--- Demo1.java
+++ Demo1.java
@@ -2,13 +2,16 @@ class A {
     public A() {
         System.out.println("in A");
     }
+    public A(int a) {
+        System.out.println("int i A ");
+    }
 }
 class B extends A {
     public B() {
         System.out.println("in B");
     }
     public B(int a) {
-        System.out.println("int in B");
+        System.out.println("int in B ");
     }
 }
 public class Demo1 {
