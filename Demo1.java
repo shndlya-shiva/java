7250ec6c535674ad6d00284850b9c420f15a9446
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-09 23:10:17 +0530

    Addind Parametirsed Construrctor

--- Demo1.java
+++ Demo1.java
@@ -7,9 +7,12 @@ class B extends A {
     public B() {
         System.out.println("in B");
     }
+    public B(int a) {
+        System.out.println("int in B");
+    }
 }
 public class Demo1 {
     public static void main(String[] args) {
-        B obj = new B();
+        B obj = new B(4);
     }
 }
