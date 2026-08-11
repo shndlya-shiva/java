a892e5fa09cce40bd2b4b439d3f31866762a269c
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-11 22:42:54 +0530

    Access Modifier

---
+++ packages/E.java
@@ -0,0 +1,10 @@
+package packages;
+// In this we will learn about packages and Access Modifiers in java.
+// Access modifiers are of diff types - public, private, default and protected.
+// Public access modifier allows the class, method or variable to be accessed from any other class or package.
+// Private access modifier allows the class, method or variable to be accessed only within the same class.
+// Default access modifier allows the class, method or variable to be accessed only within the same package.
+// Protected access modifier allows the class, method or variable to be accessed within the same package and by subclasses in other packages.
+public class E {
+    int marks = 50;
+}
