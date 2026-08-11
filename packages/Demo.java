e9bd9631de4b255e2c4db44b1977a90594fb9d89
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-11 21:28:35 +0530

    Understanding Package

---
+++ packages/Demo.java
@@ -0,0 +1,10 @@
+package packages;
+// In this we will learn about packages in java. 
+// Packages are used to group related classes and interfaces together. 
+// It helps in avoiding name conflicts and also provides access control. 
+// There are two types of packages in java: built-in packages and user-defined packages. 
+// Built-in packages are those that come with the Java Development Kit (JDK) and,
+// User-defined packages are those that we create ourselves.
+public class Demo {
+    
+}
