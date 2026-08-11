4c9b6fd31f0fe58b3591290d07322381248da1e9
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-11 22:24:22 +0530

    Understanding Packages

--- packages/Demo.java
+++ packages/Demo.java
@@ -1,4 +1,6 @@
 package packages;
+// This is package declaration for the Demo class, which is part of the 'packages' package.
+// The Demo class demonstrates the usage of the E class from the same package.
 // In this we will learn about packages in java. 
 // Packages are used to group related classes and interfaces together. 
 // It helps in avoiding name conflicts and also provides access control. 
