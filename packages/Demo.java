a803773f7bc3848310b77dc287cc63788ce561cd
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-11 22:43:06 +0530

    Understanding Packages

--- packages/Demo.java
+++ packages/Demo.java
@@ -7,6 +7,12 @@ package packages;
 // There are two types of packages in java: built-in packages and user-defined packages. 
 // Built-in packages are those that come with the Java Development Kit (JDK) and,
 // User-defined packages are those that we create ourselves.
+// Any file that exist in the package directory is part of the package and,
+// Can't be accessed outside the package unless it is declared as public.\
+// For the file to be accessed outside the package, it must be declared as public and the file name must match the class name.
+// Variable should not be declared as public but the method should be public to be accesessed outside the package.
 public class Demo {
-    
+    public static void main(String [] args) {
+
+    }
 }
