a07e14e1a1ac54f191e58225ee3a56d82e8d9771
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-11 23:19:24 +0530

    Branch Test Work

---
+++ hello.java
@@ -0,0 +1,156 @@
+package java;
+class test{
+    public static void main(String[] args){
+    // first java program
+        System.out.println("Hello World");
+    }
+}
+class test1{
+    public static void main(String[] args){
+     //int data types
+     //int stores integer values (whole numbers), without decimals, such as 123 or -123
+        int num = 5;
+        int num1 = 10;
+        int num2 = 15;
+        int result = num1 + num2 + num;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
+    }
+}
+class test2{
+    public static void main(String[] args){
+        //float data types
+        //float data types are used to store decimal numbers, such as 3.14 or -3.14 but with less precision than double
+        float num = 5.5f;
+        float num1 = 10.5f;
+        float num2 = 15.5f;
+        float result = num1 + num2 + num;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
+    }
+}
+class test3{
+    public static void main(String[] args){
+        //double data types
+        //double data types are used to store decimal numbers, such as 3.14 or -3.14
+        double num = 5.5;
+        double num1 = 11.5;
+        double num2 = 15.5;
+        double result = num1 + num2 + num;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
+    }
+}
+class test4{
+    public static void main(String[] args){
+        //char data types
+        //char data types are used to store single characters, such as 'A' or 'B'
+        char ch = 'A';
+        char ch1 = 'B';
+        char ch2 = 'C';
+        System.out.println(ch);
+        System.out.println(ch1);
+        System.out.println(ch2);
+    }
+}
+class test5{
+    public static void main(String[] args){
+        //boolean data types
+        //boolean data types are used to store true or false values
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+    }
+}
+class test6{
+    public static void main(String[] args){
+        //String data types
+        //String data types are used to store text, such as "Hello" or "World"
+        String name = "John";
+        String name1 = "Doe";
+        String name2 = "Smith";
+        System.out.println(name);
+        System.out.println(name1);
+        System.out.println(name2);
+    }
+}
+class test7{
+    public static void main(String[] args){
+        //byte data types
+        //byte data types are used to store whole numbers from -128 to 127
+        byte num = 100;
+        byte num1 = 50;
+        byte num2 = 25;
+        byte result = (byte)(num + num1 + num2);
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
+    }
+}
+class test8{
+    public static void main(String[] args){
+        //short data types
+        //short data types are used to store whole numbers from -32,768 to 32,767
+        short num = 10000;
+        short num1 = 20000;
+        short num2 = 30000;
+        short result = (short)(num + num1 + num2);
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
+    }
+}
+class test9{
+    public static void main(String[] args){
+        //long data types
+        //long data types are used to store whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long num = 1000000000L;
+        long num1 = 2000000000L;
+        long num2 = 3000000000L;
+        long result = num + num1 + num2;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The result is: " + result);
+    }
+}
+class test10 {
+    //type casting is the process of converting a variable from one data type to another.
+    // In Java, there are two types of type casting: implicit and explicit.
+    // Implicit casting is done automatically by the compiler when a smaller data type is converted to a larger data type.
+    // Explicit casting is done manually by the programmer when a larger data type is converted to a smaller data type.
+    public static void main(String[] args) {
+        //conversion of data types
+        //converting int to byte
+        int i = 267;
+        byte b = (byte) i;
+        System.out.println(b);
+        //converting byte to int
+        byte a = 10;
+        int j = a;
+        System.out.println(j);
+        //converting float to int
+        float f = 10.5f;
+        int k = (int) f;
+        System.out.println(k);
+        //converting int to float
+        int m = 15;
+        float n = m;
+        System.out.println(n);
+        //type promotion is the automatic conversion of a smaller data type to a larger data type.
+        byte num1 = 10;
+        byte num2 = 40;
+        int result = num1 * num2;
+        System.out.println(result);
+        // all the above conversions are examples of type casting and type promotion in Java.
+    }
+}
