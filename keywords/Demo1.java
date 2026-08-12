ed9bfe6bad4436258edfaaf055c35acaed18b5a7
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-12 23:44:38 +0530

    Diff Types of Keywords

---
+++ keywords/Demo1.java
@@ -0,0 +1,27 @@
+// This and Super Keyword in Java
+// This keyword is used to refer to the current class instance variable.
+// Super keyword is used to refer to the parent class instance variable.
+class A {
+    public A() {
+        super();
+        System.out.println("in A");
+    }
+    public A(int a) {
+        super();
+        System.out.println("int i A ");
+    }
+}
+class B extends A {
+    public B() {
+        System.out.println("in B");
+    }
+    public B(int a) {
+        this();
+        System.out.println("int in B ");
+    }
+}
+public class Demo1 {
+    public static void main(String[] args) {
+        B obj = new B(4);
+    }
+}
