d01d43b51c0d08ed3e23b5d8f4d3639dc901eb90
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-14 23:02:48 +0530

    Inner Class

--- InnerClass.java
+++ InnerClass.java
@@ -4,6 +4,7 @@
 // Inner classes can access all the members (including private) of the outer class
 // Inner classes are used to implement event listeners in GUI applications, callbacks, data structures like linked lists, trees, etc. &  anonymous classes in Java
 // Inner classes are of 4 types: 1. Non-static nested class (Inner class) 2. Static nested class 3. Local inner class 4. Anonymous inner class
+// 
 class E 
 {
     int age;
@@ -12,6 +13,7 @@ class E
     {
         System.out.println("In show method of outer class");
     }
+    // Non-static nested class (Inner class) is a class which is defined inside another class and is not declared as static
     class F
     {
         public void config()
@@ -19,6 +21,7 @@ class E
             System.out.println("In config method of inner class");
         }
     }
+    // Static nested class is a static class which is defined inside another class
     static class G
     {
         public void display()
@@ -42,5 +45,14 @@ public class InnerClass
         // Syntax: OuterClass.StaticInnerClass obj = new OuterClass.StaticInnerClass();
         E.G obj2 = new E.G();
         obj2.display();
+        // Creating an anonymous inner class which is a class without a name and is used to override the method of a class or interface
+        E obj3 = new E()
+        {
+            public void show()
+            {
+                System.out.println("In show method of anonymous inner class");
+            }
+        };
+        obj3.show();
     }
 }
