596b704058aa1c99d636932cbc928997cbbc1fe4
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-18 23:32:11 +0530

    add enum data type

---
+++ Constants_Enum.java
@@ -0,0 +1,12 @@
+// Enum is a special refernce Data Type used to define Constants
+
+enum Status
+{
+    Running, Failed, Pending, Success;
+}
+public class Constants_Enum {
+    public static void main(String[] args) {
+        Status s = Status.Pending; // It returns the constant values from the class
+        Status[] s1 = Status.values(); // It returns an array of all constants
+    }    
+}
