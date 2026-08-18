cd530db9f82a4864e8079e05c69c32a5b54f1a88
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-18 23:35:34 +0530

    add enum data type

--- Constants_Enum.java
+++ Constants_Enum.java
@@ -8,5 +8,11 @@ public class Constants_Enum {
     public static void main(String[] args) {
         Status s = Status.Pending; // It returns the constant values from the class
         Status[] s1 = Status.values(); // It returns an array of all constants
+        System.out.println(s);
+        System.out.println(s1); // Printing it directly will return it address value for the values loop is used
+        for (Status s2 : s1)
+        {
+            System.out.println(s2);
+        }
     }    
 }
